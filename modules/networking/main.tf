73e738e67d25927d4d33d450946b9e5e84721a80
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-28 20:41:08 -0500

    filling out networking module

---
+++ modules/networking/main.tf
@@ -0,0 +1,16 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.64.0"
+  name    = "${var.namespace}-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = data.aws_availability_zones.available.names
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  create_database_subnet_group = true
+  enable_nat_gateway           = true
+  single_nat_gateway           = true
+}
